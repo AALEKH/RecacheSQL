eb7a5f257fa2084d5ef2f019705adbf57707d18a
aalekh <aalekh1993@rediffmail.com>
2015-06-19 00:25:57 +0530

    Combining the feature with UDF's

--- recachesql.c
+++ recachesql.c
@@ -1,10 +1,18 @@
 #include "recachesql.h"
 
-/* For Redis Operation */
+/* Generic C Header's */
+#include <my_global.h>
+#include <mysql.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <ctype.h>
 
+/* For Redis Operation */
 #include "hiredis/hiredis.h"
 #include "hiredis/async.h"
-#include "hiredis/adapters/ae.h"
+#include "hiredis/adapters/libevent.h"
 
 /**
  * Allocate unused space for a string with a specific size and null terminate it.
@@ -162,6 +170,73 @@ char *copy_argname(char *att, unsigned long length)
 	return str;
 }
 
+void finish_with_error(MYSQL *con)
+{
+  fprintf(stderr, "%s\n", mysql_error(con));
+  mysql_close(con);
+  exit(1);        
+}
+
+/*
+* Simple String Concatenation Function
+*/
+void concat(char s1[], char s2[]) {
+   int i, j;
+ 
+   i = strlen(s1);
+ 
+   for (j = 0; s2[j] != '\0'; i++, j++) {
+      s1[i] = s2[j];
+   }
+ 
+   s1[i] = '\0';
+}
+
+/* 
+* Function to be used for construct Redis List from MySQL Table Column's 
+*/
+void get_values(MYSQL *con, char row[200], char t_name[250] ) {
+  
+  redisContext *c;
+  redisReply *reply;
+  char vi[1024] = "SELECT ";
+  int j = 0;
+  char str[500];
+  c = redisConnect("127.0.0.1", 6379);
+  if(c!=NULL && c->err){
+    printf("Error: %s\n", c->errstr);
+  }
+  concat(vi, row);
+  concat(vi, " from ");
+  concat(vi, t_name);
+  printf("%s\n", row);
+  if (mysql_query(con, vi)) 
+  {
+      finish_with_error(con);
+  }
+  
+  MYSQL_RES *result = mysql_store_result(con);
+  if (result == NULL) 
+  {
+      finish_with_error(con);
+  }
+
+  int num_fields = mysql_num_fields(result);
+
+  MYSQL_ROW row_name;
+  
+  while ((row_name = mysql_fetch_row(result))) 
+  {  
+      for(int i = 0; i < num_fields;) 
+      { 
+        j = j + 1;
+        redisCommand(c,"LPUSH %s-%s '%s'", t_name, row, row_name[i]);
+        i = i + 1;
+      } 
+  }
+
+}
+
 my_bool recachesql_init(UDF_INIT *initid, UDF_ARGS *args, char *message) {
 	if (args->arg_count != 1) {
        		strcpy(message, "recachesql can only accept one argument");
@@ -177,20 +252,57 @@ my_bool recachesql_init(UDF_INIT *initid, UDF_ARGS *args, char *message) {
 }
 
 int recachesql(UDF_INIT *initid, UDF_ARGS *args, char *is_null, char *error) {
-	redisContext *c;
-	redisReply *reply;
-	c = redisConnect("127.0.0.1", 6379);
-	if(c!=NULL && c->err){
-		printf("Error: %s\n", c->errstr);
-	}
-	reply = redisCommand(c,"SET %s %s", "foo", "hello world");
-	printf("SET: %s\n", reply->str);
-	freeReplyObject(reply);
+/*	args->args[0];
+	args->args[0];
+*/	
+	MYSQL *con = mysql_init(NULL);
 
-	reply = redisCommand(c,"GET foo");
-	printf("GET foo: %s\n", reply->str);
-	freeReplyObject(reply);
-	redisFree(c);
+	if (con == NULL) {
+
+	    fprintf(stderr, "mysql_init() failed\n");
+	    exit(1);
+	}  
+	  
+	if (mysql_real_connect(con, "localhost", "root", "", args->args[0], 0, NULL, 0) == NULL) {
+
+	    finish_with_error(con);
+	}
+	//SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'testdb' AND TABLE_NAME = 'Cars';
+
+	char a[1024] = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = '";
+	concat(a, args->args[0]);
+	concat(a, "' AND TABLE_NAME = '");
+	concat(a, args->args[1]);
+	concat(a, "'");
+	if (mysql_query(con, a)) {
+
+	      finish_with_error(con);
+	}
+	  
+	MYSQL_RES *result = mysql_store_result(con);
+	  
+	if (result == NULL) {
+
+	    finish_with_error(con);
+	}
+
+	int num_fields = mysql_num_fields(result);
+
+	MYSQL_ROW row;
+	  
+	while ((row = mysql_fetch_row(result))) {
+
+	    for(int i = 0; i < num_fields; i++) {
+
+	        get_values(con, row[i], args->args[1]); 
+	    } 
+	        printf("\n"); 
+	}
+	  
+	mysql_free_result(result);
+	mysql_close(con);
+	  
+	exit(0);
 	return 0;
 }
 
