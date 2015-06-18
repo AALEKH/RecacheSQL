f28f5bd212d774b3593c7ad920f3616bf03c7274
aalekh <aalekh1993@rediffmail.com>
2015-06-18 11:18:01 +0530

    Added Redis functionality

--- sqltest.c
+++ sqltest.c
@@ -1,6 +1,12 @@
 #include <my_global.h>
 #include <mysql.h>
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include "hiredis/hiredis.h"
+#include "hiredis/async.h"
+#include "hiredis/adapters/libevent.h"
 
 void finish_with_error(MYSQL *con)
 {
@@ -22,11 +28,19 @@ void concat(char s1[], char s2[]) {
 }
 
 void get_values(MYSQL *con, char row[20], char t_name[25] ) {
-
+  
+  redisContext *c;
+  redisReply *reply;
   char vi[1024] = "SELECT ";
+  char str[500];
+  c = redisConnect("127.0.0.1", 6379);
+  if(c!=NULL && c->err){
+    printf("Error: %s\n", c->errstr);
+  }
   concat(vi, row);
   concat(vi, " from ");
   concat(vi, t_name);
+  printf("%s\n", row);
   if (mysql_query(con, vi)) 
   {
       finish_with_error(con);
@@ -45,9 +59,14 @@ void get_values(MYSQL *con, char row[20], char t_name[25] ) {
   
   while ((row_name = mysql_fetch_row(result))) 
   { 
-      for(int i = 0; i < num_fields; i++) 
+      for(int i,j = 0; i < num_fields; i++, j++) 
       { 
-          printf("%s ", row_name[i] ? row_name[i] : "NULL"); 
+        concat(row, sprintf(str, "%d", j));
+        printf("see this: %s\n", t_name);
+        reply = redisCommand(c,"HMSET %s %s %s", t_name, row, row_name[i]);
+        printf("%s\n", reply);
+        freeReplyObject(reply);
+        //printf("%s ", row_name[i] ? row_name[i] : "NULL"); HMSET tutorialspoint name-aa "aalekh" 
       } 
           printf("\n\n\n\n"); 
   }
